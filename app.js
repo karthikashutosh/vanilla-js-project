476285d926c37df2c99a4ff75e53eef61da3a36e
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-05-24 10:53:45 +0530

    innerText Done

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var clickButton = document.querySelector("#btn-default");
 var inputText = document.querySelector("#input");
 var outputText = document.querySelector("output-text");
- 
-console.log(inputText);
 
-function clickHandler(){
-    console.log(inputText.value);
+function clickHandler() {}
 
-}
+outputText.innerHTML = inputText.value;
 
-clickButton.addEventListener("click",clickHandler)
+clickButton.addEventListener("click", clickHandler);
